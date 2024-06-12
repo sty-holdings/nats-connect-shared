e63b9ef5613fc5703144e661684db84fda9cb254
Scott Yacko <scott.yacko@sty-holdings.com>
2024-06-12 12:23:00 -0700

    Added GetTeamReply

--- reply.go
+++ reply.go
@@ -4,6 +4,25 @@ import (
 	"time"
 )
 
+type GetTeamReply struct {
+	Error struct {
+		ErrorAdditionalInfo string `json:"error_additional_info"`
+		ErrorFilename       string `json:"error_filename"`
+		ErrorFunctionName   string `json:"error_function_name"`
+		ErrorLineNumber     int    `json:"error_line_number"`
+		ErrorMessage        string `json:"error_message"`
+	} `json:"error"`
+	Response struct {
+		Created time.Time `json:"created"`
+		Id      string    `json:"id"`
+		Limits  struct {
+			DisablePrometheusFederation bool `json:"disable_prometheus_federation"`
+			NumUsers                    int  `json:"num_users"`
+		} `json:"limits"`
+		Name string `json:"name"`
+	} `json:"response"`
+}
+
 type ListTeamsReply struct {
 	Error struct {
 		ErrorAdditionalInfo string `json:"error_additional_info"`
