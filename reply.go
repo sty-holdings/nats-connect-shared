2778d22d80eac1ee457439fbce2ec94ddec58424
Scott Yacko <scott.yacko@sty-holdings.com>
2024-06-12 15:14:58 -0700

    Added ListSystemAccountInfoReply

--- reply.go
+++ reply.go
@@ -251,6 +251,25 @@ type ListPersonalAccessTokensReply struct {
 	} `json:"response"`
 }
 
+type ListSystemAccountInfoReply struct {
+	Error struct {
+		ErrorAdditionalInfo string `json:"error_additional_info"`
+		ErrorFilename       string `json:"error_filename"`
+		ErrorFunctionName   string `json:"error_function_name"`
+		ErrorLineNumber     int    `json:"error_line_number"`
+		ErrorMessage        string `json:"error_message"`
+	} `json:"error"`
+	Response struct {
+		Items []struct {
+			AccountPublicKey string `json:"account_public_key"`
+			Id               string `json:"id"`
+			IsScpAccount     bool   `json:"is_scp_account"`
+			IsSystemAccount  bool   `json:"is_system_account"`
+			Name             string `json:"name"`
+		} `json:"items"`
+	} `json:"response"`
+}
+
 type ListSystemsReply struct {
 	Error struct {
 		ErrorAdditionalInfo string `json:"error_additional_info"`
