4ba7f4e5eec9d670396696d590c6109c2d4f5cf6
Scott Yacko <scott.yacko@sty-holdings.com>
2024-06-12 15:22:52 -0700

    Added ListAccounts

--- reply.go
+++ reply.go
@@ -204,6 +204,124 @@ type GetVersionReply struct {
 	} `json:"response"`
 }
 
+type ListAccounts struct {
+	Error struct {
+		ErrorAdditionalInfo string `json:"error_additional_info"`
+		ErrorFilename       string `json:"error_filename"`
+		ErrorFunctionName   string `json:"error_function_name"`
+		ErrorLineNumber     int    `json:"error_line_number"`
+		ErrorMessage        string `json:"error_message"`
+	} `json:"error"`
+	Response struct {
+		Items []struct {
+			AccountPublicKey string `json:"account_public_key"`
+			Claims           struct {
+				Iat  int    `json:"iat"`
+				Iss  string `json:"iss"`
+				Jti  string `json:"jti"`
+				Name string `json:"name"`
+				Nats struct {
+					Authorization struct {
+						AuthUsers interface{} `json:"auth_users"`
+					} `json:"authorization"`
+					DefaultPermissions struct {
+						Pub struct {
+						} `json:"pub"`
+						Sub struct {
+						} `json:"sub"`
+					} `json:"default_permissions"`
+					Description string `json:"description,omitempty"`
+					Imports     []struct {
+						Account      string `json:"account"`
+						LocalSubject string `json:"local_subject"`
+						Name         string `json:"name"`
+						Subject      string `json:"subject"`
+						Type         string `json:"type"`
+					} `json:"imports"`
+					Limits struct {
+						Conn         int `json:"conn"`
+						Data         int `json:"data"`
+						Exports      int `json:"exports"`
+						Imports      int `json:"imports"`
+						Leaf         int `json:"leaf"`
+						Payload      int `json:"payload"`
+						Subs         int `json:"subs"`
+						TieredLimits struct {
+							R1 struct {
+								Consumer           int   `json:"consumer"`
+								DiskMaxStreamBytes int64 `json:"disk_max_stream_bytes"`
+								DiskStorage        int64 `json:"disk_storage"`
+								MaxBytesRequired   bool  `json:"max_bytes_required"`
+								Streams            int   `json:"streams"`
+							} `json:"R1"`
+						} `json:"tiered_limits"`
+						Wildcards bool `json:"wildcards"`
+					} `json:"limits"`
+					SigningKeys []string `json:"signing_keys"`
+					Type        string   `json:"type"`
+					Version     int      `json:"version"`
+				} `json:"nats"`
+				Sub string `json:"sub"`
+			} `json:"claims"`
+			ClaimsInfo struct {
+				JsEnabled bool `json:"js_enabled"`
+			} `json:"claims_info"`
+			Created         time.Time `json:"created"`
+			Id              string    `json:"id"`
+			IsScpAccount    bool      `json:"is_scp_account"`
+			IsSystemAccount bool      `json:"is_system_account"`
+			Jwt             string    `json:"jwt"`
+			JwtSettings     struct {
+				Authorization struct {
+					AuthUsers interface{} `json:"auth_users"`
+				} `json:"authorization"`
+				Description string `json:"description,omitempty"`
+				Limits      struct {
+					Conn         int `json:"conn"`
+					Data         int `json:"data"`
+					Exports      int `json:"exports"`
+					Imports      int `json:"imports"`
+					Leaf         int `json:"leaf"`
+					Payload      int `json:"payload"`
+					Subs         int `json:"subs"`
+					TieredLimits struct {
+						R1 struct {
+							Consumer           int   `json:"consumer"`
+							DiskMaxStreamBytes int64 `json:"disk_max_stream_bytes"`
+							DiskStorage        int64 `json:"disk_storage"`
+							MaxBytesRequired   bool  `json:"max_bytes_required"`
+							Streams            int   `json:"streams"`
+						} `json:"R1"`
+					} `json:"tiered_limits"`
+					Wildcards bool `json:"wildcards"`
+				} `json:"limits"`
+			} `json:"jwt_settings"`
+			JwtSyncError  string `json:"jwt_sync_error"`
+			JwtSyncStatus string `json:"jwt_sync_status"`
+			Name          string `json:"name"`
+			NscManaged    bool   `json:"nsc_managed"`
+			System        struct {
+				ConnectionType       string   `json:"connection_type"`
+				HttpGatewayUrl       string   `json:"http_gateway_url"`
+				Id                   string   `json:"id"`
+				IsTenant             bool     `json:"is_tenant"`
+				JetstreamDomain      string   `json:"jetstream_domain"`
+				JetstreamEnabled     bool     `json:"jetstream_enabled"`
+				JetstreamTiers       []string `json:"jetstream_tiers"`
+				Name                 string   `json:"name"`
+				ServerUrls           string   `json:"server_urls"`
+				State                string   `json:"state"`
+				UserJwtExpiresInSecs int      `json:"user_jwt_expires_in_secs"`
+			} `json:"system"`
+			Team struct {
+				Id   string `json:"id"`
+				Name string `json:"name"`
+			} `json:"team"`
+			UserJwtExpiresInSecs int `json:"user_jwt_expires_in_secs"`
+		} `json:"items"`
+	} `json:"response"`
+}
+
 type ListInfoAppUsersTeamReply struct {
 	Error struct {
 		ErrorAdditionalInfo string `json:"error_additional_info"`
